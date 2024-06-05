7f7f31469b58231bc968a5372edf010f2b70dd2d
mi nombre <mi@email.com>
2024-06-05 00:39:33 -0300

    sixth commit Primer form listo. Al buscador le falta un try para camadas inexistentes

--- ContainerCoderProject/CoderApp/urls.py
+++ ContainerCoderProject/CoderApp/urls.py
@@ -1,13 +1,25 @@
 
 #from django.contrib import admin
 from django.urls import path
-from CoderApp.views import inicio, cursos, entregables, estudiantes, profesores
+from CoderApp.views import (
+    inicio,
+    cursos,
+    formCurso,
+    entregables,
+    estudiantes,
+    profesores,
+    busqueda_camada,
+    busqueda
+)
 
 
 urlpatterns = [
-    path('', inicio),
-    path('cursos/', cursos),
-    path('entregables/', entregables),
-    path('estudiantes/', estudiantes),
-    path('profesores/', profesores),
+    path('', inicio, name='Inicio'),
+    path('cursos/', cursos, name='Cursos'),
+    path('curso-formulario/', formCurso, name='FormCurso'),
+    path('entregables/', entregables, name='Entregables'),
+    path('estudiantes/', estudiantes, name='Estudiantes'),
+    path('profesores/', profesores, name='Profesores'),
+    path('busqueda-camada/', busqueda_camada, name='BusquedaCamada'),
+    path('busqueda/', busqueda, name='BusquedaCurso'),
 ]
--- ContainerCoderProject/CoderApp/views.py
+++ ContainerCoderProject/CoderApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from .models import Curso
+from .forms import CursoFormulario
 
 #view para el inicio
 def inicio(req):
@@ -7,21 +9,66 @@ def inicio(req):
 
 def cursos(req):
 
-    return render(req, 'cursos.html')
+    lista = Curso.objects.all()
+
+    return render(req, 'cursos.html', {"lista_cursos": lista})
+
+def formCurso(req):
+
+    if req.method == 'POST':
+
+        miFormulario = CursoFormulario(req.POST)
+
+        if miFormulario.is_valid():
+            
+            data = miFormulario.cleaned_data
+
+            nuevo_curso = Curso(nombre=data['curso'], camada=data['camada'])
+            nuevo_curso.save()
+            
+            return render(req, 'inicio.html', {"message": "Curso creado con éxito"})
+        else:
+            return render(req, 'inicio.html', {"message": "Datos no validos"})
+    else:
+
+        miFormulario = CursoFormulario()
+
+        return render(req, 'curso_formulario.html', {"miFormulario": miFormulario})
+
+
 
 def entregables(req):
 
-    return render(req, 'entregables.html')
+    return render(req, 'entregables.html',{})
 
 def estudiantes (req):
 
-    return render(req, 'estudiantes.html')
+    return render(req, 'estudiantes.html', {})
 
 def profesores(req):
 
-    return render(req, 'profesores.html')
+    return render(req, 'profesores.html', {})
 
 
+
+def busqueda_camada(req):
+
+    return render(req, 'busqueda_camada.html', {})
+
+def busqueda(req):
+    if req.GET["camada"]:
+
+        camada = req.GET["camada"]
+
+        curso = Curso.objects.get(camada=camada)
+
+        return render(req, 'resultado_busqueda.html', {"curso": curso, "camada": camada})
+    
+    else:
+
+        return render(req, 'inicio.html', {"message": "Error en el dato de la camada"})
+
+ 
 
 
 
